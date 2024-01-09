0cf230aeb9afb67a1a70fee1cb195112e126a2e0
Junior <verasengenheiro@gmail.com>
2024-01-09 16:09:46 -0300

    first commit

---
+++ RPA - Criando um bot com python.py
@@ -0,0 +1,15 @@
+#RPA - Criando um bot com python
+
+# Observações
+## P/ pausar a automacao basta levar a seta do mouse ao canto superior esquerdo da tela e esperar 5 s
+
+
+
+
+
+
+
+
+import pyautogui
+
+pyautogui.press("Iniciar")
